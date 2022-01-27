739363a6715f609419ae1897f7ec55b1e8e0deaf
alexmaga17 <alexmagalhaes17@gmail.com>
2022-01-27 01:02:32 +0000

    description

--- src/store/index.js
+++ src/store/index.js
@@ -21,10 +21,10 @@ export default new Vuex.Store({
 							header: 'PRECISAS DE AJUDA EM FOTOGRAFIA?',
 							video: '../../assets/bgVideoLanding.mp4',
 						},
-						photos: [],
+						photos: ['https://quantocustaviajar.com/blog/wp-content/uploads/2020/03/como-tirar-fotos-sozinha-em-viagens-14.jpg'],
 						shortDescription:
 							'Eu consigo fazer uma sessão de fotografias dentro de água.',
-						description: '',
+						description: 'Estarei disponível para sessões de fotografia em qualquer ocasião ou altura do ano.Tiro fotos a qualquer coisa, tenho muitos trabalhos anteriores realizados com estudantes.Não hesites em contactar-me!!',
 						faq: [
 							{
 								title: 'Em que dias da semana estás disponível para tirar fotos?',
@@ -58,14 +58,14 @@ export default new Vuex.Store({
 							header: 'PRECISAS DE AJUDA COM UM DESENHO?',
 							video: '../../assets/bgVideoLanding3.mp4',
 						},
-						photos: [],
+						photos: ['https://img.elo7.com.br/product/zoom/2CCA5F0/desenho-realista-desenho-profissional.jpg'],
 						shortDescription:
-							'Consigo fazer o desenho do teu website em pouco tempo.',
-						description: '',
+							'Consigo fazer o desenho de qualquer coisa.',
+						description: 'Tenho experiência em desenho abstrato, mas faço qualquer tipo de desenho á tua escolha, sou sem dúvida a tua melhor opção no que toca a desenho.Não procures mais, contacta me e vamos deixar esse projeto perfeito!',
 						faq: [
 							{
-								title: '',
-								description: '',
+								title: 'Demoro muito tempo a entregar os desenhos?',
+								description: 'Costumo ser rápido a não ser que esteja ocupado com trabalhos da faculdade!Mas não te preocupes , terás o teu desenho a tempo!',
 							},
 						],
 						overallRating: 5,
@@ -95,14 +95,14 @@ export default new Vuex.Store({
 							header: 'PRECISAS DE UM MODELO?',
 							video: '../../assets/bgVideoLanding5.mp4',
 						},
-						photos: [],
+						photos: ['http://blog.selfiemodels.com.br/wp-content/uploads/2017/05/modelo-fotografico-1200x800.jpg?x65681'],
 						shortDescription:
 							'Consigo tornar o teu projeto viral em pouco tempo.',
-						description: '',
+						description: 'Sou uma modelo profissional agenciada há 3 anos, faço sessões para os teus projetos em qualquer sítio, basta contactar-me. Podes verificar mais os meus trabalhos em todas as minhas redes sociais.',
 						faq: [
 							{
-								title: '',
-								description: '',
+								title: 'Estás dísponivel para sessões a qualquer altura?',
+								description: 'Em princípio sim , mas nada que uma mensagem não resolva melhor! :)',
 							},
 						],
 						overallRating: 4,
@@ -132,14 +132,14 @@ export default new Vuex.Store({
 							header: 'PRECISAS DE AJUDA COM UM VÍDEO?',
 							video: '../../assets/bgVideoLanding2.mp4',
 						},
-						photos: [],
+						photos: ['https://animalleestudio.com.br/wp-content/uploads/2019/09/video-promocional.jpg'],
 						shortDescription:
 							'Farei um vídeo para o teu projeto á tua maneira.',
-						description: '',
+						description: 'Sou experiente em filmagens e edição de vídeo em diversas plataformas tais como o Adobe Premier, Sony Vegas, Entre outras... Para além disso, também costuma fazer animações 3D para vídeos promocionais infantis. Sente-te á vontade para me Contactar!',
 						faq: [
 							{
-								title: '',
-								description: '',
+								title: 'Tens todo o equipamento para a gravação de vídeos promocionais?',
+								description: 'Sim, não te preocupes, todo o material que necessites, eu terei!',
 							},
 						],
 						overallRating: 3,
@@ -169,14 +169,14 @@ export default new Vuex.Store({
 							header: 'PRECISAS DE AJUDA COM UM VÍDEO?',
 							video: '../../assets/bgVideoLanding2.mp4',
 						},
-						photos: [],
+						photos: ['https://clickandplaylive.com/wp-content/uploads/2020/11/APAVT-Promo-Tutorial-Oculos-360-Video-Promocional.jpg'],
 						shortDescription:
 							'Eu consigo editar o teu vídepo promocional em qualquer plataforma.',
-						description: '',
+						description: 'Tenho 3 cursos de edição de vídeo, estarei disponível para editar o teu vídeo em qualquer formato, a qualquer altura, fiz imensos vídeos promocionais, alguns deles até passam na TV!',
 						faq: [
 							{
-								title: '',
-								description: '',
+								title: 'Consegues fazer um vídeo promocional em menos de uma semana?',
+								description: 'Dependendo da dimansão do vídeo...mas sim, não considero nada impossível!',
 							},
 						],
 						overallRating: 4,
@@ -206,14 +206,14 @@ export default new Vuex.Store({
 							header: 'PRECISAS DE AJUDA EM FOTOGRAFIA?',
 							video: '../../assets/bgVideoLanding.mp4',
 						},
-						photos: [],
+						photos: ['https://pdimagemecarreira.com/principal/wp-content/uploads/2019/01/271651-desafios-profissionais-voce-esta-preparado-para-2019.jpg'],
 						shortDescription:
 							'Tenho disponiblilidade para fotografar qualquer coisa(pessoas/objetos).',
-						description: '',
+						description: 'Estarei disposto para fotografar em qualquer parte do país.Tenho 2 cursos diplomados em edição de fotografia.Sou a melhor escolha desta plataforma no que toca a fotografia.Contacta-me!!',
 						faq: [
 							{
-								title: '',
-								description: '',
+								title: 'Tens como te deslocar a qualquer ponto do país?',
+								description: 'Sim, tenho carro, isso não é problema!',
 							},
 						],
 						overallRating: 5,
