e0b9a8a7120db35119e21b1aac4626eb34be5fc2
alexmaga17 <alexmagalhaes17@gmail.com>
2022-03-16 16:15:02 +0000

    .

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="mainContainer">
-        
+        <div class="container">
+            <div class="headerContainer">
+                <h1>Mensagens</h1>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,6 +15,26 @@
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.mainContainer {
+	margin-top: 100px;
+	font-family: 'Gilroy', sans-serif;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background-color: black;
+	color: white;
+}
 
+.container {
+	display: flex;
+	justify-content: center;
+	width: 90%;
+}
+
+h1 {
+	font-weight: 900;
+	margin: 0;
+	font-size: 70px;
+}
 </style>
