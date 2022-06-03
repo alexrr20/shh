069b0d40654e762d46d6520ab0c58f0d97d870e5
alexrr20 <alex11.bessa@gmail.com>
2022-06-03 15:08:22 +0100

    Messages Start

---
+++ src/components/Message.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="messageContainer">
+      <div class="messageHeader">
+          <h2>Maria</h2>
+          <h3>14 de Dezembro de 2021 às 18:30h</h3>
+      </div>
+      <div class="messageContent">
+          Olá, como estás? Antes de mais queria agradecer por me contactares, eu queria saber se tens alguma ideia para o poster? 
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Message"
+}
+</script>
+
+<style>
+    .messageContainer{
+        background-color: #E4E4E4;
+        padding:30px 40px 30px 40px;
+        margin-top: 30px;
+    }
+
+    h2{
+        font-weight: 800;
+        font-size:18px;
+        margin:0;
+    }
+
+    h3{
+        font-weight: 700;
+        font-size:14px;
+        color:#848484;
+        margin:0;
+    }
+</style>
--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -1,20 +1,61 @@
 <template>
     <div class="mainContainer">
-        <div class="container">
-            <div class="headerContainer">
-                <h1>Mensagens</h1>
-            </div>
-            <div class="assideContainer">
+        <div class="contentContainer">
+            <aside>
                 <h4>Contactos</h4>
+                <div class="contactsContainer">
+                    <div class="contactContainer">
+                        <div class="contactTop">
+                            <div class="nomeContainer"><span>Nome1</span></div>
+                            <div class="categoriaContainer"><span>Categoria1</span></div>
+                            </div>
+                        <div class="contactBottom"><span>Online há 3 dias</span></div>
+                    </div>
+                    <span class="divider"></span>
+                    <div class="contactContainer">
+                        <div class="contactTop">
+                            <div class="nomeContainer"><span>Nome1</span></div>
+                            <div class="categoriaContainer"><span>Categoria1</span></div>
+                            </div>
+                        <div class="contactBottom"><span>Online há 3 dias</span></div>
+                    </div>
+                    <span class="divider"></span>
+                    <div class="contactContainer">
+                        <div class="contactTop">
+                            <div class="nomeContainer"><span>Nome1</span></div>
+                            <div class="categoriaContainer"><span>Categoria1</span></div>
+                            </div>
+                        <div class="contactBottom"><span>Online há 3 dias</span></div>
+                    </div>
+                </div>
+            </aside>
+            <div class="mainContentContainer">
+                <div class="chatMainContainer">
+                    <div class="chatHeader">
+                        <div class="chatHeaderLeft">              
+                            <h2>Maria</h2>
+                            <h4>Desenho</h4>
+                        </div>
+                        <div class="chatHeaderRight">
+                            <span>avaliar</span>
+                        </div>
+    
+                    </div>
+                    <div class="messagesContainer">
+                        <Message/>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import Message from '../components/Message.vue';
+
     export default {
         name:'Messages',
-        components:{},
+        components:{ Message },
     }
 </script>
 
@@ -29,9 +70,96 @@
     min-height: 80vh;
 }
 
+.contentContainer{
+    display:flex;
+    width: 90%;
+    max-width: 1300px;
+    justify-content: space-between;
+}
+
 .headerContainer {
 	font-weight: 900;
 	font-size: 35px;
     margin-right: 900px;
 }
+
+aside{
+    width:180px;
+}
+
+.divider {
+	display: inline-block;
+	background-color: #e4e4e4;
+	width: 100%;
+	height: 1px;
+}
+
+.contactContainer:first-child{
+    margin-top:30px;
+}
+
+.contactContainer{
+    display: flex;
+    flex-direction: column;
+    cursor: pointer;
+    margin-top: 7px;
+}
+
+.contactTop{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.categoriaContainer span{
+    font-size:15px;
+    font-weight: 800;
+}
+
+.contactBottom{
+    display:flex;
+    align-items:flex-start
+}
+
+.contactBottom span{
+    font-size:9px;
+    color: #BDBDBD;
+    font-weight: 600;
+}
+
+.nomeContainer span{
+    font-size: 25px;
+    font-weight: 800;
+}
+
+.mainContentContainer{
+    width:76%;
+}
+
+.chatHeader{
+    display:flex;
+    align-items: flex-end;
+    justify-content: space-between;
+}
+
+.chatHeaderLeft{
+    display:flex;
+    align-items: flex-end;
+}
+
+.chatHeaderRight{
+}
+
+h2{
+    font-size:60px;
+    margin:0;
+    height:60px;
+    font-weight: 800;
+}
+
+h4{
+    margin:0;
+}
+
+
 </style>
